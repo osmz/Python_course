485db47e5e9b99c95def91914e08e049b3ef0bd8
Oscar Morales <oscar.s.morales.z@usp.br>
2022-10-06 10:10:23 -0500

    Modify Example_2.py, add general information

--- Python_course/Example_2.py
+++ Python_course/Example_2.py
@@ -1,4 +1,5 @@
 # Problema que solicita al usuario una cantidad de pesos, una tasa de interés y un número de años. Muestra por pantalla en cuánto se habrá convertido el capital inicial transcurrido esos años si cada año se aplica la tasa de interés introducida.
+
 # Recuerde que un capital de C pesos a un interés del x por cien durante n años se convierte en C*(1+ x/100)^n pesos. Pruebe su programa sabiendo que una cantidad de 10.000 oesis al 4.5% de interés anual se convierte en 24.1117,14 pesos al cabo de 20 años.
 
 capital = float(input('Ingrese el capital inicial: '))
